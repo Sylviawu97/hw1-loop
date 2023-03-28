35bb986a67ef95199f95516f5aba4d4c26dcba6a
Wu Sylvia <>
2023-03-28 18:48:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+//1
+let numberOfStudents = 17;
+
+if (numberOfStudents > 20){
+  console.log("Classroom too small")
+}
+else{
+  console.log("Classroom size acceptable")
+}
+
+//2
+let numberOfHouses = 5;
+
+if (numberOfHouses >2){
+  let numberOfHouses = true;
+  console.log("you need to sell more houses")
+  
+}
+else if (numberOfHouses <= 2){
+  console.log ("seems reasonable")
+  } 
+
+//3
+  let bill=20
+
+  if (bill > 15){
+    console.log("we should split the bill")
+  }
+  else{
+    console.log("I’ll get the bill this time")
+  }
+
+//4
+let score =70
+if (score<=50){
+  console.log("Not to good")
+}
+else if(score<=60){
+  console.log("ehhhh")
+}
+else if(score<=70){
+  console.log("Keep trying")
+}
+else if(score<=80){
+  console.log("nice!")
+}
+else if (score<=90){
+  console.log("Doing good!")
+}
+else{
+  console.log("over 90!great!")
+}
+
+//5.1
+
+let dog = "corgi"
+if (dog== "corgi"){
+  console.log("yeah,you got the right puppy")
+}
+else if(dog =="husky"){
+  console.log("oh,thu puppy might be naughty")
+}
+else{
+  console.log("No other puppies.")
+}
+
+//5.2
+
+let gender = "FEMALE"
+ if(gender == "FEMALE"){
+   console.log("Hello,Ms.")
+ }
+else if(gender == "MALE"){
+  console.log("Hello,Mr.")
+}
+else {
+  console.log("Hello,dear.")
+}
+
+//5.3
+let priceoflunch =10
+if (priceoflunch<=5){
+  console.log("The lunch is good to try.")
+}
+else{
+  console.log("Sooooo pricy.")
+}
+
+//5.4
+let age=3
+if(age<3){
+  console.log("Too young for school")
+}
+else if(age>=3 && age<=5){
+  console.log("Cool for preschool")
+}
+else if(age>5 && age<=13){
+  console.log("cool for elemantary")
+}
+else{
+  console.log("wrong place")
+}
+
+
+//5.5
+let cupOfAlchol =5
+if (cupOfAlchol <=1){
+  console.log("still awake")
+} 
+else {
+  console.log("STop drinking")
+}
